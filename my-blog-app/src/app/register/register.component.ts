8251a1b30e0df4f85f080c7c59beda6e60c9c9b1
Parikshit Thatte <parikshitthatte23@gmail.com>
2021-08-23 10:29:07 +0530

    redirect added after successful registration

--- my-blog-app/src/app/register/register.component.ts
+++ my-blog-app/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthenticationService } from '../authentication.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
@@ -9,12 +11,13 @@ import { AuthenticationService } from '../authentication.service';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private authenticationService:AuthenticationService) { }
+  constructor(private authenticationService:AuthenticationService, private _router: Router) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(form:NgForm ) {
     this.authenticationService.registerNewUser(form.value.userEmail, form.value.userPassword);
+    this._router.navigateByUrl('/login');
   }
 }
